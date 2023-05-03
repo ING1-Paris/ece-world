d4b031b586a77e3be1c47e248f843f9c7d09edfb
Kylian Pinto <kylian.pinto@edu.ece.fr>
2023-05-04 00:43:58 +0200

    changement

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <allegro.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+BITMAP *bitmap[3];
+BITMAP *bitmap2[3];
+BITMAP *bitmap3[3];
 int main(void) {
     printf("bonjour !\n");
     printf("salut comment");
