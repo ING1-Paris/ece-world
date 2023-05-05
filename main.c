b2fb56acfc547287690292c6f861a164e1ff0415
Kylian Pinto <kylian.pinto@edu.ece.fr>
2023-05-05 18:10:23 +0200

    sous programme pour le jeu jakpot

--- main.c
+++ main.c
@@ -4,7 +4,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+void shuffleBitmaps(BITMAP **bitmaps, int count) {
+    int i, j;
+    BITMAP *temp;
 
+    //srand(time(NULL)); // Initialisation de la graine aléatoire
+
+    // Mélange du tableau
+    for (i = count - 1; i > 0; i--) {
+        j = rand() % (i + 1);
+        temp = bitmaps[i];
+        bitmaps[i] = bitmaps[j];
+        bitmaps[j] = temp;
+    }
+}
 BITMAP *bitmap[3];
 BITMAP *bitmap2[3];
 BITMAP *bitmap3[3];
