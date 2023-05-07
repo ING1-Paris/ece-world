0f09c25fe73529a11e59ec38f29e7425ed22ce11
Kylian Pinto <kylian.pinto@edu.ece.fr>
2023-05-07 22:51:20 +0200

    sous programme pour le jeu jakpot

--- main.c
+++ main.c
@@ -21,8 +21,24 @@ void shuffleBitmaps(BITMAP **bitmaps, int count) {
 BITMAP *bitmap[3];
 BITMAP *bitmap2[3];
 BITMAP *bitmap3[3];
+
+
 int main(void) {
     printf("bonjour !\n");
     printf("salut comment");
+    bitmap[0] = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\bar jakpot.bmp", NULL);
+    bitmap[1] = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\bar jakpot.bmp", NULL);
+    bitmap[2] = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\bar jakpot.bmp", NULL);
+    BITMAP *background=load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\images rouleau.bmp", NULL);
+    bitmap2[0]=load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\image piece .bmp", NULL);
+    bitmap2[1]=load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\image piece .bmp", NULL);
+    bitmap2[2] = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\image piece .bmp", NULL);
+
+    bitmap3[0] = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\image piece .bmp", NULL);
+    bitmap3[1] = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\image piece .bmp", NULL);
+    bitmap3[2] = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\image piece .bmp", NULL);
+
+    BITMAP* manivelle = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\image manivelle.bmp", NULL);
+    BITMAP* manivelleretour = load_bitmap("C:\\Users\\kylia\\CLionProjects\\test 4\\manivelle retour.bmp", NULL);
     return 0;
 }END_OF_MAIN();
