ae983eabf8b0b82e9f435ad67b193f4ffa3d902b
Kycks912004 <131270426+Kycks912004@users.noreply.github.com>
2023-05-08 22:05:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int est_sur_rondin(int x_grenouille, int y_grenouille, int x_rondin, int y_rondin, int largeur_rondin, int hauteur_rondin) {
     int sur_rondin = 0;
     if (y_grenouille == y_rondin) {
