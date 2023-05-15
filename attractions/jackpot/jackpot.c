8baa3e4ad67277b5f978d5dcc1644a0666dca1d9
Kycks912004 <131270426+Kycks912004@users.noreply.github.com>
2023-05-15 21:27:53 +0200

    machine a japot et scroll des bitmaps vers le bas

--- attractions/jackpot/jackpot.c
+++ attractions/jackpot/jackpot.c
@@ -41,4 +41,129 @@ int main() {
     shuffleBitmaps(bitmap, 3);
     shuffleBitmaps(bitmap2, 3);
     shuffleBitmaps(bitmap3, 3);
+    
+    int bitmap_index = 0;
+    int bitmap_index2 = 0;
+    int bitmap_index3 = 0;
+    draw_sprite(screen,bitmap[0],0,130);
+    draw_sprite(screen,bitmap2[0],355,130);
+    draw_sprite(screen,bitmap3[0],710,130);
+    rectfill(screen, 0, 0, SCREEN_WIDTH, 140, makecol(0, 0, 0)); // top
+    rectfill(screen, 0, SCREEN_HEIGHT - 90, SCREEN_WIDTH, SCREEN_HEIGHT, makecol(0, 0, 0)); // bottom
+    rectfill(screen, 0, 0, 10, SCREEN_HEIGHT, makecol(0, 0, 0)); // left
+    rectfill(screen, SCREEN_WIDTH - 25, 0, SCREEN_WIDTH, SCREEN_HEIGHT, makecol(0, 0, 0));// right
+    //draw_sprite(screen,bitmap[0],0,130);
+    //draw_sprite(screen,bitmap2[0],355,130);
+    //draw_sprite(screen,bitmap3[0],710,130);
+
+    draw_sprite(screen, manivelle, 1060, 193);
+    int k=0;
+    player.playerticket=5;
+    while (!key[KEY_ESC]) {
+        // Le bouton gauche de la souris est enfoncé;
+        //k=k+0.001;
+        if(((mouse_b & 1&&mouse_x>106 && mouse_x < 1060+manivelle->w && mouse_y >= 300&& mouse_y < 300+manivelle->h)||(clic_gauche_precedent==1))&&k<=754) {
+                //clear_to_color(screen_bitmap, makecol(0,0,0));
+
+                clic_gauche_precedent=1;
+                //int k=0;
+                k=k+1;printf("%d\n",k);
+                if (k==755){
+                printf("je l'ai trouve las sjnjchdvcdnvkdjvkjkvjfjvfvl");
+                player.playerticket-=1;
+                int color1 = getpixel(screen, 175, SCREEN_HEIGHT-manivelleretour->h);
+                int color2=getpixel(screen, 530, SCREEN_HEIGHT-manivelleretour->h);
+                int color3=getpixel(screen, 885, SCREEN_HEIGHT-manivelleretour->h);
+                if ((color1==color2)&&(color1==color3)&&(color2==color3)){
+                    printf("la partie est gagne");
+                    printf("%d   %d   %d",color1,color2,color3);
+                    //piece=16637518  7=15343395
+                    if (color1==16637518){
+                        player.playerticket=player.playerticket+2;
+                    }
+                    if (color1==15343395){
+                        player.playerticket=player.playerticket-1;
+                    }
+                    printf(" le nombre de tich=ket du joueur est de %d",player.playerticket);
+                }
+                else{
+                    printf("partie perdue");
+                }
+            }
+
+
+        if(k<500){
+            scroll_offset =scroll_offset +SCROLL_SPEED+25;
+            scroll_offset2 =scroll_offset2 +SCROLL_SPEED+20;
+            scroll_offset3 =scroll_offset3 +SCROLL_SPEED+15;
+
+        }
+        else if (k>500 &&k <750){
+            scroll_offset =scroll_offset +SCROLL_SPEED+5;
+            scroll_offset2 =scroll_offset2 +SCROLL_SPEED+11;
+            scroll_offset3 =scroll_offset3 +SCROLL_SPEED+19;
+        }
+        else if (k>750 && k<1000){
+            scroll_offset =scroll_offset +SCROLL_SPEED+6;
+            scroll_offset2 =scroll_offset2 +SCROLL_SPEED+5;
+            scroll_offset3 =scroll_offset3 +SCROLL_SPEED+5;
+        }
+        else{
+            scroll_offset =scroll_offset +SCROLL_SPEED;
+            scroll_offset2 =scroll_offset2 +SCROLL_SPEED;
+            scroll_offset3 =scroll_offset3 +SCROLL_SPEED;
+        }
+
+
+
+
+        // If the first bitmap goes out of screen, reset scroll offset and bitmap index
+        if (scroll_offset >= BITMAP_HEIGHT) {
+            scroll_offset = 0;
+            bitmap_index = (bitmap_index + 1) % 3;
+        }
+        if (scroll_offset2 >= BITMAP_HEIGHT) {
+                scroll_offset2 = 0;
+                bitmap_index2 = (bitmap_index2 + 1) % 3;
+        }
+        if (scroll_offset3>= BITMAP_HEIGHT) {
+                scroll_offset3 = 0;
+                bitmap_index3 = (bitmap_index3 + 1) % 3;
+        }
+
+        // Draw bitmaps to buffer
+        clear_bitmap(buffer[buffer_index]);
+
+        // Draw bitmaps one after the other in a loop
+
+
+        int y = -scroll_offset;
+        int y2 = -scroll_offset2;
+        int y3 = -scroll_offset3;
+
+        for (int i = 0; i < 4; i++) {
+            draw_sprite(buffer[buffer_index], bitmap[(bitmap_index + i) % 3], 0, y);
+            draw_sprite(buffer[buffer_index], bitmap2[(bitmap_index2 + i) % 3], 355, y2);
+            draw_sprite(buffer[buffer_index], bitmap3[(bitmap_index3 + i) % 3], 710, y3);
+            y += BITMAP_HEIGHT;
+            y2 += BITMAP_HEIGHT;
+            y3 += BITMAP_HEIGHT;
+        }
+        rectfill(buffer[buffer_index], 0, 0, SCREEN_WIDTH, 140, makecol(0, 0, 0)); // top
+        rectfill(buffer[buffer_index], 0, SCREEN_HEIGHT - 90, SCREEN_WIDTH, SCREEN_HEIGHT, makecol(0, 0, 0)); // bottom
+        rectfill(buffer[buffer_index], 0, 0, 10, SCREEN_HEIGHT, makecol(0, 0, 0)); // left
+        rectfill(buffer[buffer_index], SCREEN_WIDTH - 25, 0, SCREEN_WIDTH, SCREEN_HEIGHT, makecol(0, 0, 0));// right
+        draw_sprite(buffer[buffer_index],manivelleretour,1060,300);
+
+
+        // Flip buffer to screen
+        //acquire_screen();
+        blit(buffer[buffer_index], screen, 0, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        //release_screen();
+
+        // Switch buffer
+        buffer_index = (buffer_index + 1) % BUFFER_SIZE;
+
+        rest(1);
+            }
 
