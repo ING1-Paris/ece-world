ca95396f9bfbd072f3ebf597a002b7ef5e9e8302
redahassani75 <131269170+redahassani75@users.noreply.github.com>
2023-05-16 19:28:20 +0200

    Update riviere.c
    Mise en place du déplacement complet des rondins , 3 rondins par rangée sur 4 rangées, et mise en place de la condition de défaite ou de victoire avec attribution d'un ticket si le joueur gagne, et mise en place du temps de victoire (si joueur tombe temps=0s). Déplacement de la grenouille et gestion de si la grenouille reste sur le rondin et dépasse l'écran et si la grenouille tombe

--- attractions/riviere/riviere.c
+++ attractions/riviere/riviere.c
@@ -5,17 +5,9 @@
 #define SCREEN_H 480
 #define LOG_SPEED 2
 #define FROG_H 32
+#define FROG_W 32
 #define LOG_WIDTH 128
 #define LOG_HEIGHT 64
-/*typedef struct grenouille{
-    int x;
-    int y;
-}grenouille;
-typedef struct rondin{
-    int x;
-    int y;
-    int direction;
-}rondin;*/
 int est_sur_rondin(int x_grenouille, int y_grenouille, int x_rondin, int y_rondin, int largeur_rondin, int hauteur_rondin) {
     int sur_rondin = 0;
     if (y_grenouille>= y_rondin && y_grenouille<=y_rondin+hauteur_rondin) {
@@ -27,76 +19,156 @@ int est_sur_rondin(int x_grenouille, int y_grenouille, int x_rondin, int y_rondi
 }
 
 int main() {
-    // Initialiser Allegro
+    srand(time(NULL));
     allegro_init();
     install_keyboard();
     set_color_depth(desktop_color_depth());
     set_gfx_mode(GFX_AUTODETECT_WINDOWED, SCREEN_W, SCREEN_H, 0, 0);
-    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,SCREEN_W,SCREEN_H,0,0)!=0)
-    {
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, SCREEN_W, SCREEN_H, 0, 0) != 0) {
         allegro_message("prb gfx mode");
         allegro_exit();
         exit(EXIT_FAILURE);
     }
 
     // Charger les sprites à partir de fichiers bitmap
-    BITMAP *frog_bmp = load_bitmap("C:\\Users\\redah\\projetalgogrenouille\\grenouilleoff.bmp", NULL);
-    BITMAP *log_bmp = load_bitmap("C:\\Users\\redah\\projetalgogrenouille\\bois.bmp", NULL);
-    BITMAP *fond= load_bitmap("C:\\Users\\redah\\projetalgogrenouille\\fond.bmp",NULL);
+    BITMAP *frog_bmp = load_bitmap("../grenouilleoff.bmp", NULL);
+    BITMAP *log_bmp = load_bitmap("../bois.bmp", NULL);
+    BITMAP *fond = load_bitmap("../fond.bmp", NULL);
     if (!frog_bmp || !log_bmp || !fond) {
         allegro_message("Impossible de charger les sprites.");
         return 1;
     }
-// Déclaration des variables
+
+
+    // Créer les sprites de grenouille et de rondins de bois
     int frog_x = SCREEN_W / 2;
     int frog_y = SCREEN_H - FROG_H;
-    int score = 0;        
-    int log_x[12] = { 0, 0, 0, 0,0, 0, 0, 0,0,0,0,0};
-    int log_y[12] = { 120, 180, 240, 300 ,120, 180, 240, 300,120, 180, 240, 300 ,120 };
-    int log_speed[12] = { LOG_SPEED, -LOG_SPEED, LOG_SPEED, -LOG_SPEED ,LOG_SPEED, -LOG_SPEED, LOG_SPEED, -LOG_SPEED,LOG_SPEED, -LOG_SPEED, LOG_SPEED, -LOG_SPEED };
-     //initialisation rondin
+
+
+    // Faire sauter la grenouille ,deplacement grenouille,Déclaration des variables
+    int log_x[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int log_y[12] = {120, 180, 240, 300, 120, 180, 240, 300, 120, 180, 240, 300, 120};
+    int log_speed[12] = {LOG_SPEED, -LOG_SPEED, LOG_SPEED, -LOG_SPEED, LOG_SPEED, -LOG_SPEED, LOG_SPEED, -LOG_SPEED,
+                         LOG_SPEED, -LOG_SPEED, LOG_SPEED, -LOG_SPEED};
     for (int i = 0; i < 4; i++) {
-        log_x[i] = SCREEN_W/3;
-        log_x[i+4]= 2*SCREEN_W/3;
-        log_x[i+8]=SCREEN_W;
-        log_speed[i] = (rand() % 4) + 1;
-        log_speed[i+4] =log_speed[i] ;
-        log_speed[i+8] =log_speed[i];
-}
-    //création des buffer, initialisation du temps,de la vitesse de la grenouille et du score
+        log_x[i] = SCREEN_W / 3;
+        log_x[i + 4] = 2 * SCREEN_W / 3;
+        log_x[i + 8] = SCREEN_W;
+        log_speed[i] = (rand() % 8) + 1;
+        log_speed[i + 4] = log_speed[i];
+        log_speed[i + 8] = log_speed[i];
+    }
+
     int frog_speed = 8;
-    BITMAP *buffer= create_bitmap(SCREEN_W,SCREEN_H);
+    BITMAP *buffer = create_bitmap(SCREEN_W, SCREEN_H);
     clear_bitmap(buffer);
-    clock_t start_time = clock(); // temps de début du jeu
-    float elapsed_time = 0.0f; // temps écoulé
-    float scorefinal[10];
-    float max_score=0;
-    float score=0;
+    clock_t start_time = clock(); 
+    float elapsed_time = 0.0f;
+    float score_final[10];
+    float max_score_ = 0;
+    float score_ = 0;
     int num_played = 0;
+
 // Boucle de jeu
-        while (!key[KEY_ESC]) {
-            clear_to_color(buffer, makecol(0, 0, 0));
-            elapsed_time = (float)(clock() - start_time) / CLOCKS_PER_SEC;
-            // Dessin du fond d'écran
-            rectfill(buffer, 0, 0, SCREEN_W, SCREEN_H / 4, makecol(0, 255, 0));
-            rectfill(buffer, 0, SCREEN_H - SCREEN_H / 4, SCREEN_W, SCREEN_H, makecol(0, 255, 0));
-            rectfill(buffer,0,SCREEN_H/4,SCREEN_W,SCREEN_H-SCREEN_H/4, makecol(0,0,255));
 
-           
-            textprintf_ex(screen, font, 10, 10, makecol(0, 0, 0), -1, "Temps : %d", score);
-            vsync();
-            acquire_screen();
-            blit(buffer, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
-            release_screen();
+    while (!key[KEY_ESC]) {
+        clear_to_color(buffer, makecol(0, 0, 0));
+        elapsed_time = (float) (clock() - start_time) / CLOCKS_PER_SEC;
+        // Dessin du fond d'écran
+        rectfill(buffer, 0, 0, SCREEN_W, SCREEN_H / 4, makecol(0, 255, 0));
+        rectfill(buffer, 0, SCREEN_H - SCREEN_H / 4, SCREEN_W, SCREEN_H, makecol(0, 255, 0));
+        rectfill(buffer, 0, SCREEN_H / 4, SCREEN_W, SCREEN_H - SCREEN_H / 4, makecol(0, 0, 255));
+
+        for (int i = 0; i < 4; i++) {
+            log_x[i] += log_speed[i];
+            log_x[i + 4] += log_speed[i];
+            log_x[i + 8] += log_speed[i];
+            if (log_x[i] > SCREEN_W) {
+                log_x[i] = 0 - LOG_WIDTH;
+                log_speed[i] = (rand() % 4) + 1;
+                log_speed[i + 4] = log_speed[i];
+            }
+            if (log_x[i + 4] > SCREEN_W) {
+                log_x[i + 4] = 0 - LOG_WIDTH;
+                log_speed[i+4] = (rand() % 4) + 1;
+                log_speed[i] = log_speed[i + 4];
+            }
+            if (log_x[i + 4] < 0 - LOG_WIDTH) {
+                log_x[i + 4] = 0 - LOG_WIDTH;
+                log_speed[i+4] = (rand() % 4) + 1;
+                log_speed[i] = log_speed[i + 4];
+            }
+            if (log_x[i] < 0 - LOG_WIDTH) {
+                log_x[i] = SCREEN_W;
+                log_speed[i] = -((rand() % 4) + 1);
+                log_speed[i + 4] = log_speed[i];
+            }
+            if (log_x[i + 8] > SCREEN_W) {
+                log_x[i + 8] = 0 - LOG_WIDTH;
+                log_speed[i+8] = (rand() % 4) + 1;
+                log_speed[i] = log_speed[i + 8];
+            }
+            if (log_x[i + 8] < 0 - LOG_WIDTH) {
+                log_x[i + 8] = 0 - LOG_WIDTH;
+                log_speed[i+8] = (rand() % 4) + 1;
+                log_speed[i] = log_speed[i + 8];
+            }
+            log_speed[i + 4] = log_speed[i];
+            log_speed[i + 8] = log_speed[i];
         }
-// faire fonction grenouille qui tombe à l'eau (perdu avec temps =0)
-// faire fonction gagner si la grenouille arrive à destination avec le temps
-// faire fonction afficher le score du joueur
+        for (int i = 0; i < 12; i++) {
+            masked_blit(log_bmp, buffer, 0, 0, log_x[i], log_y[i], LOG_WIDTH, LOG_HEIGHT);
+        }
 
+        int sur_rondin = 0;
+        for (int i = 0; i < 12; i++) {
+            if (est_sur_rondin(frog_x, frog_y, log_x[i], log_y[i], LOG_WIDTH, LOG_HEIGHT)) {
+                sur_rondin = 1;
+                frog_x += log_speed[i];
+            }
+        }
 
-
+        if (sur_rondin) {
+            if (key[KEY_LEFT] && frog_x > 0) {
+                frog_x -= frog_speed;
+            } else if (key[KEY_RIGHT] && frog_x < SCREEN_W - 32) {
+                frog_x += frog_speed;
+            } else if (key[KEY_UP] && frog_y > 0) {
+                frog_y -= frog_speed;
+            } else if (key[KEY_DOWN] && frog_y < SCREEN_H - 32) {
+                frog_y += frog_speed;
+            }
+        } else {
+            if (key[KEY_LEFT] && frog_x > 0) {
+                frog_x -= frog_speed;
+            } else if (key[KEY_RIGHT] && frog_x < SCREEN_W - 32) {
+                frog_x += frog_speed;
+            } else if (key[KEY_UP] && frog_y > 0) {
+                frog_y -= frog_speed;
+            } else if (key[KEY_DOWN] && frog_y < SCREEN_H - 32) {
+                frog_y += frog_speed;
+            }
+        }
+        if ((sur_rondin == 0 && frog_y + FROG_H > SCREEN_H / 4 + 16 && frog_y + 16 < SCREEN_H - SCREEN_H / 4) ||
+            (sur_rondin == 1 && frog_x > SCREEN_W - FROG_W)) {
+            allegro_message("Perdu ! Votre temps est de 0 secondes");
+            allegro_exit();
+        }
+        if (sur_rondin == 0 && frog_y + FROG_H < SCREEN_H / 4 && frog_x < SCREEN_W && frog_x > 0) {
+            allegro_message("Bien joue ! votre temps est de %.2f secondes\n \tVous avez gagné un ticket ! ", elapsed_time);
+            allegro_exit();
+        }
+        textprintf_ex(buffer, font, 10, 10, makecol(0, 0, 0), -1, "Temps ecoule : %.2f secondes", elapsed_time);
+        //textprintf_ex(buffer, font, SCREEN_H - 20, 10, makecol(0, 0, 0), -1, "tentative numéro %d",);
+        vsync();
+        draw_sprite(buffer, frog_bmp, frog_x, frog_y);
+        blit(buffer, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+        rest(20);
+        num_played++;
+        score_ = elapsed_time;
+        if (score_ < max_score_) {
+            max_score_ = score_;
+        }
+    }
 
 }END_OF_MAIN()
-
-
-
