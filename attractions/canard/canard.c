5b5a2aec69ea653f64e0f4207e0418725a9c2f64
redahassani75 <131269170+redahassani75@users.noreply.github.com>
2023-05-20 13:53:38 +0200

    Update canard.c
    Rajout collision entre les canards et j'ai diminué le nombre de canards dans l'eau ( de 20 à 10) car sinon il y avait trop de collision et de bugs et en conséquence j'ai diminué le temps avant que le jeu s'arrête vu qu'il y a moins de canards.

--- attractions/canard/canard.c
+++ attractions/canard/canard.c
@@ -8,26 +8,33 @@ typedef struct {
     int speed_x, speed_y;
     int is_alive;
 } Duck;
+int canardcollision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
+    return !(x1 + w1 < x2 || x2 + w2 < x1 || y1 + h1 < y2 || y2 + h2 < y1);
+}
 int main() {
+    // Initialisation d'Allegro
     allegro_init();
     install_keyboard();
     install_mouse();
     set_color_depth(16);
     set_gfx_mode(GFX_AUTODETECT_WINDOWED, SCREEN_W, SCREEN_H, 0, 0);
+
     BITMAP *canard = load_bitmap("../canard.bmp", NULL);
     BITMAP *harpon = load_bitmap("../harpon.bmp", NULL);
     BITMAP *filet = load_bitmap("../filet.bmp",NULL);
+
+    // Variables de jeu
     int score = 0;
     int ducks_caught = 0;
     int time_elapsed = 0;
     int fishing_rod_x = SCREEN_W / 2;
     int fishing_rod_y = SCREEN_H - 100;
-    int canard_x[20];
-    int canard_y[20];
-    int vitesse_canard[20];
+    int canard_x[10];
+    int canard_y[10];
+    int vitesse_canard[10];
     clock_t start_time = clock();
     float elapsed_time = 0.0f;
-    for(int i=0;i<20;i++){
+    for(int i=0;i<10;i++){
         vitesse_canard[i]=(rand() %10)-5;
         canard_x[i]= rand() % SCREEN_W +vitesse_canard[i];
         canard_y[i] = rand() % (SCREEN_H - 100)+vitesse_canard[i];
@@ -35,20 +42,21 @@ int main() {
     BITMAP *buffer = create_bitmap(SCREEN_W, SCREEN_H);
     BITMAP *buffer_menu = create_bitmap(SCREEN_W, SCREEN_H);
 
-    Duck ducks[20];
+    Duck ducks[10];
 
-    for (int i = 0; i < 20; i++) {
+    
+    for (int i = 0; i < 10; i++) {
         ducks[i].x = rand() % SCREEN_W;
         ducks[i].y = rand() % SCREEN_H;
         ducks[i].speed_x = rand() % 6 - 3;
         ducks[i].speed_y = rand() % 6 - 3;
         ducks[i].is_alive = 1;
     }
-    int pos_x[20];
-    int pos_y[20];
-    for (int i = 0; i < 20; i++) {
-        pos_x[i]=20+rand()%60;
-        pos_y[i]=20+rand()%60;
+    int pos_x[10];
+    int pos_y[10];
+    for (int i = 0; i < 10; i++) {
+        pos_x[i]=10+rand()%60;
+        pos_y[i]=10+rand()%60;
     }
     while(!key[KEY_ENTER]) {
 
@@ -59,6 +67,7 @@ int main() {
         rectfill(buffer_menu, 0, 0, SCREEN_W, SCREEN_W, makecol(0, 0, 255));
         rectfill(buffer_menu, button_xs, button_y, button_xs + button_x, button_y + button_height, makecol(255, 255, 255));
         textout_centre_ex(buffer_menu, font, "Play", SCREEN_W / 2, SCREEN_H / 2, makecol(0, 0, 0), -1);
+        
         vsync();
         blit(buffer_menu, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
     }
@@ -74,7 +83,7 @@ int main() {
         }
 
         if (mouse_b & 1) {
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < 10; i++) {
                 if (ducks[i].is_alive) {
                     int distance_x = ducks[i].x - mouse_x;
                     int distance_y = ducks[i].y - mouse_y;
@@ -82,10 +91,11 @@ int main() {
                     if (distance < 40) {
                         ducks[i].is_alive = 0;
                         score++;
+
                     }
                 }}}
         draw_sprite(buffer,filet,0,0);
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             ducks[i].x += ducks[i].speed_x;
             ducks[i].y += ducks[i].speed_y;
             if (ducks[i].x < 0 || ducks[i].x > SCREEN_W) {
@@ -94,9 +104,17 @@ int main() {
             if (ducks[i].y < 0 || ducks[i].y > SCREEN_H) {
                 ducks[i].speed_y = -ducks[i].speed_y;
             }
+            for (int j = 0; j < 10; j++) {
+                if (i != j && canardcollision(ducks[i].x, ducks[i].y, 40, 40, ducks[j].x, ducks[j].y, 40, 40)) {
+                    ducks[i].speed_x = -ducks[i].speed_x;
+                    ducks[i].speed_y = -ducks[i].speed_y;
+                    ducks[j].speed_x = -ducks[j].speed_x;
+                    ducks[j].speed_y = -ducks[j].speed_y;
+                }
+            }
         }
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
 
             if (ducks[i].is_alive==0) {
                 draw_sprite(buffer,canard,pos_x[i],pos_y[i]);
@@ -104,17 +122,18 @@ int main() {
             if (ducks[i].is_alive) {
             draw_sprite(buffer, canard, ducks[i].x, ducks[i].y);
         }}
-        if(elapsed_time>15.00){
+        if(elapsed_time>10.00){
             allegro_message("Bien joue ! vous avez attrapé %d canards\n \tVous avez gagné un ticket ! ", score);
             allegro_exit();
         }
         textprintf_centre_ex(screen, font, SCREEN_W / 2, 10, makecol(255, 255, 255),-1, "Score: %d", score);
-
         draw_sprite(buffer, harpon, mouse_x-harpon->w, mouse_y);
-        textprintf_ex(buffer, font, 10, 10, makecol(0, 0, 0), -1, "Temps ecoule : %.2f secondes", 15.00-elapsed_time);
+        textprintf_ex(buffer, font, 10, 10, makecol(0, 0, 0), -1, "Temps ecoule : %.2f secondes", 10.00-elapsed_time);
         vsync();
         blit(buffer, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
     }
+
+    // Libération des ressources graphiques
     destroy_bitmap(canard);
     destroy_bitmap(harpon);
     destroy_bitmap(buffer);
@@ -122,3 +141,5 @@ int main() {
     allegro_exit();
     return 0;
 }END_OF_MAIN()
+ 
+
